432d6ff4b330b9a764a3fa71723a1c2cf26c7008
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-12-04 11:51:07 -0800

    Allow any positive int

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,32 +1,24 @@
 def caesar_cipher(string, shift)
+  shift %= 26
   arr = string.split('')
   cipher = arr.map do |char|
     if char >= "A" && char <= "Z"
       num = char.ord - "A".ord + 1 + shift
-      if num > 26
-        num -= 26
-        num
-      end
-      (num + 64).chr
+      ((num%26) + 64).chr
     elsif char >= "a" && char <= "z"
       num = char.ord - "a".ord + 1 + shift
-      if num > 26
-        num -= 26
-        num
-      end
-      (num + 96).chr
+      ((num%26) + 96).chr
     else
       char
     end
   end
   string = cipher.join('')
-  puts string
+  string
 end
-
-# caesar_cipher("Hello my name is mud!", 7)
 
 puts "Enter a string:"
 str = gets.chomp
-puts "Enter an integer (1-26):"
+puts "Enter an integer:"
 int = gets.chomp.to_i
-caesar_cipher(str, int)
+cipher = caesar_cipher(str, int)
+puts "Your message, encrypted:\n#{cipher}"
