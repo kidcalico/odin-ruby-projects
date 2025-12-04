7915dee8f78b08fe3e8f51f4dea28647ff2e0da0
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-12-04 11:25:07 -0800

    Write initial caesar cipher code
    Currently only works for shift values between 1 and 26.

---
+++ caesar-cipher/cipher.rb
@@ -0,0 +1,26 @@
+def caesar_cipher(string, shift)
+  arr = string.split('')
+  cipher = arr.map do |char|
+    if char >= "A" && char <= "Z"
+      num = char.ord - "A".ord + 1 + shift
+      if num > 26
+        num -= 26
+        num
+      end
+      (num + 64).chr
+    elsif char >= "a" && char <= "z"
+      num = char.ord - "a".ord + 1 + shift
+      if num > 26
+        num -= 26
+        num
+      end
+      (num + 96).chr
+    else
+      char
+    end
+  end
+  string = cipher.join('')
+  puts string
+end
+
+caesar_cipher("Hello my name is mud!", 5)
