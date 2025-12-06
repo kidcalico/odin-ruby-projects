9472cdf5b5cf9783a1356dfb2340b29d19875cbb
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-12-05 22:18:56 -0800

    Refactor and randomize stocks array
    Refactored stock_picker to use .each_with_index instead of for loops.
    Also added a randomized set of numbers to use for stocks for
    troubleshooting.

--- stock-picker/stockpicker.rb
+++ stock-picker/stockpicker.rb
@@ -1,4 +1,10 @@
-stocks = [17,3,6,9,15,8,6,1,14]
+# stocks = [17,3,6,9,15,8,6,1,10]
+
+# Generate an array of ten random integers between 0 and 20 to be used
+# as stock prices:
+stocks = []
+10.times {|i| stocks.push(rand(20))}
+p stocks
 
 def stock_picker(arr)
 
@@ -10,15 +16,30 @@ def stock_picker(arr)
   # their indexes in predefined variables, and return those values 
   # after cycling through all possibilities
 
-  for i in 0..arr.length-1
-    for j in i+1..arr.length-1
-      diff = arr[j] - arr[i]
-      if diff > max_profit
-        max_profit = diff
-        buy_sell = [i, j]
+  # Third attempt, using each_with_index. So far this is my favorite solution.
+
+  arr.each_with_index do |max_price, max_index|
+    arr.each_with_index do |min_price, min_index|
+      if min_index < max_index
+        profit = max_price - min_price
+        if profit > max_profit
+          max_profit = profit
+          buy_sell = [min_index, max_index]
+        end
       end
     end
   end
+
+  # Second solution, using for loops
+  # for i in 0..arr.length-1
+  #   for j in i+1..arr.length-1
+  #     diff = arr[j] - arr[i]
+  #     if diff > max_profit
+  #       max_profit = diff
+  #       buy_sell = [i, j]
+  #     end
+  #   end
+  # end
 
   puts "Maximum profit: #{max_profit}"
   puts "Days to buy and sell: #{buy_sell}"
