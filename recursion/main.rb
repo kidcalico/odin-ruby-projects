50ae999fbc8e2140c790fc14944b02913d515af0
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-01-26 14:50:04 -0800

    Write initial methods for fibs and fibs_rec
    Wrote method fib, which returns an array of integers based on an input
    integer, created using the Array#each method. Also wrote a recursive
    method, significantly more challenging. I can't help but knowing that
    there is a more elegant solution, but until I find it, this is what I
    have.

---
+++ recursion/main.rb
@@ -0,0 +1,26 @@
+def fibs(num)
+  seq = []
+  (0..num-1).each do |var|
+    seq << var if var < 2
+    seq << seq[var - 1] + seq[var - 2] if var >= 2
+  end
+  seq
+end
+
+def fibs_rec(num, seq = [])
+  if num == 1
+    seq << 0
+  elsif num == 2
+    fibs_rec(num-1, seq)
+    seq << 1
+  else
+    fibs_rec(num-1, seq)
+    seq << seq[-1] + seq[-2]
+  end
+
+  return seq
+end
+
+x = 9
+puts "Recursive function: #{fibs_rec(x)}"
+puts "Looping function: #{fibs(x)}"
