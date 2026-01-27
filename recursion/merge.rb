128728f147c3f29eba28174e2d3aa1e6317a9cab
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2026-01-26 19:13:48 -0800

    Refactor merge.rb and fibs.rb
    Removed excess code and randomized test output on fibs.rb. Changed
    merging method from using if/else conditional to case/when. Also
    directly assigned each half of the array, sent recursively through the
    merge method, to the variables a and b respectively. Yay for fewer lines
    of code. ...how else can I improve?

--- recursion/merge.rb
+++ recursion/merge.rb
@@ -1,62 +1,23 @@
 def merge_sort(array)
   return array if array.length == 1
-  a = array[0...array.length/2]
-  b = array[array.length/2..-1]
-  
-  a = merge_sort(a)
-  p a
-  b = merge_sort(b)
-  p b
-  
-  c = []
+  a = merge_sort(array[0...array.length/2])
+  b = merge_sort(array[array.length/2..-1])
+  merged = []
 
   for i in (0..array.length-1) do
-    if a == []
-      c << b.shift
-    elsif b == []
-      c << a.shift
-    elsif a[0] < b[0]
-      c << a.shift
-    else
-      c << b.shift
+    case
+    when a == [] then merged << b.shift
+    when b == [] then merged << a.shift
+    when a[0] < b[0] then merged << a.shift
+    else merged << b.shift
     end
   end
 
-  # i = 0
-  # j = 0
-  # k = 0
-  
-  # m = a.length
-  # n = b.length
-  
-  # while(i < m && j < n) do
-  #   if a[i] < b[j]
-  #     c[k] = a[i]
-  #     i += 1
-  #   else
-  #     c[k] = b[j]
-  #     j += 1
-  #   end
-  #   k += 1
-  # end
-
-  # while i < m do
-  #   c[k] = a[i]
-  #   k += 1
-  #   i += 1
-  # end
-
-  # while j < n do
-  #   c[k] = b[j]
-  #   k += 1
-  #   j += 1
-  # end
-
-  return c
+  return merged
 end
 
-arr = [7, 4, 99, 54, 5, 11, 10, 4, 20, 61, 21]
-# arr = []
-# 11.times { arr << rand(100)+1 }
-puts "Orig: #{arr}"
-print merge_sort(arr)
+arr = []
+x = rand(20) + 1
+x.times { arr << rand(100)+1 }
+puts "Original array: #{arr}"
+print "Sorted array: #{merge_sort(arr)}"
